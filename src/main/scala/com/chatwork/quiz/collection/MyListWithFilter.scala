0ab922e49668c565085c35342efda1ffd38ea0e8
hayasshi <daisuke.hayashi@future-shop.co.jp>
2015-05-08 17:30:27 +0900

    use match case in MyListWithFilter

--- src/main/scala/com/chatwork/quiz/collection/MyListWithFilter.scala
+++ src/main/scala/com/chatwork/quiz/collection/MyListWithFilter.scala
@@ -2,32 +2,23 @@ package com.chatwork.quiz.collection
 
 sealed trait MyListWithFilter[+A] {
 
-  def withFilter(f: A => Boolean): MyListWithFilter[A] = MyNilWithFilter
-
-  def map[B](f: A => B): MyList[B] = MyNil
-
-  def flatMap[B](f: A => MyList[B]): MyList[B] = MyNil
-
-}
-
-case class MyConsWithFilter[A](mc: MyCons[A], mf: A => Boolean) extends MyListWithFilter[A] {
-
-  override def withFilter(f: A => Boolean): MyListWithFilter[A] = MyConsWithFilter(mc, (x: A) => mf(x) && f(x))
-
-  override def map[B](f: A => B): MyList[B] = {
-    mc.foldRight(MyNil: MyList[B]) { (a, b) =>
-      if (mf(a)) f(a) :: b
-      else b
-    }
+  def withFilter(f: A => Boolean): MyListWithFilter[A] = this match {
+    case MyConsWithFilter(myList, myFilter) => MyConsWithFilter(myList, (a: A) => myFilter(a) && f(a))
+    case MyNilWithFilter                    => MyNilWithFilter
   }
 
-  override def flatMap[B](f: A => MyList[B]): MyList[B] = {
-    mc.foldRight(MyNil: MyList[B]) { (a, b) =>
-      if (mf(a)) f(a) ++ b
-      else b
-    }
+  def map[B](f: A => B): MyList[B] = this match {
+    case MyConsWithFilter(myList, myFilter) => myList.foldRight(MyNil: MyList[B])((a, b) => if (myFilter(a)) { f(a) :: b } else { b })
+    case MyNilWithFilter                    => MyNil
+  }
+
+  def flatMap[B](f: A => MyList[B]): MyList[B] = this match {
+    case MyConsWithFilter(myList, myFilter) => myList.foldRight(MyNil: MyList[B])((a, b) => if (myFilter(a)) { f(a) ++ b } else { b })
+    case MyNilWithFilter                    => MyNil
   }
 
 }
+
+case class MyConsWithFilter[A](myList: MyList[A], myFilter: A => Boolean) extends MyListWithFilter[A]
 
 object MyNilWithFilter extends MyListWithFilter[Nothing]
